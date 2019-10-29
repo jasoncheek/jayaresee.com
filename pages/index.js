6df3ab6b25a5c6b810a641b51a7d3fbcb2de362d
jrcheek@gmail.com <jrcheek@gmail.com>
2019-10-29 16:36:26 -0500

    update ga

--- pages/index.js
+++ pages/index.js
@@ -49,13 +49,16 @@ const Home = (props) => {
       <Head>
         <title>Jason Cheek</title>
         <link rel='icon' href='/favicon.ico' />
-        <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','UA-44641857-3');`}} />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-44641857-3"></script>
+        <script dangerouslySetInnerHTML={{__html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'UA-44641857-3');
+          `}}
+        />
       </Head>
-      <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-44641857-3" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`}} />
       <Page>
         <Intro className="order-0"> 
           <Header>
