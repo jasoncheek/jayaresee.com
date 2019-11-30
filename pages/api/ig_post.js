2964d89442480ce0c96fd14c7291af4287dd1f02
Jason Cheek <jrcheek@gmail.com>
2019-11-30 16:05:33 -0600

    update ig api endpoint

--- pages/api/ig_post.js
+++ pages/api/ig_post.js
@@ -13,5 +13,5 @@ export default async(req, res) => {
 
   res.setHeader('Content-Type', 'application/json')
   res.statusCode = 200
-  res.end(JSON.stringify(ig_post))
+  res.json(JSON.stringify(ig_post))
 }
